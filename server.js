13df7295041706715465f83befe8da372f75b464
kolawoletech <kolawolegolulana@gmail.com>
2020-01-19 15:16:37 +0200

    authentication , request and middleware

--- server.js
+++ server.js
@@ -2,80 +2,29 @@ var express = require('express');
 var graphqlHTTP = require('express-graphql');
 var { buildSchema } = require('graphql');
 
-// Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
-  input MessageInput {
-    content: String
-    author: String
-  }
-
-  type Message {
-    id: ID!
-    content: String
-    author: String
-  }
-
   type Query {
-    getMessage(id: ID!): Message
-  }
-
-  type Mutation {
-    createMessage(input: MessageInput): Message
-    updateMessage(id: ID!, input: MessageInput): Message
+    ip: String
   }
 `);
 
-// If Message had any complex fields, we'd put them on this object.
-class Message {
-  constructor(id, {content, author}) {
-    this.id = id;
-    this.content = content;
-    this.author = author;
-  }
+const loggingMiddleware = (req, res, next) => {
+  console.log('ip:', req.ip);
+  next();
 }
 
-// Maps username to content
-var fakeDatabase = {};
-
 var root = {
-  getMessage: ({id}) => {
-    if (!fakeDatabase[id]) {
-      throw new Error('no message exists with id ' + id);
-    }
-    return new Message(id, fakeDatabase[id]);
-  },
-  createMessage: ({input}) => {
-    // Create a random id for our "database".
-    var id = require('crypto').randomBytes(10).toString('hex');
-
-    fakeDatabase[id] = input;
-    return new Message(id, input);
-  },
-  updateMessage: ({id, input}) => {
-    if (!fakeDatabase[id]) {
-      throw new Error('no message exists with id ' + id);
-    }
-    // This replaces all old data, but some apps might want partial update.
-    fakeDatabase[id] = input;
-    return new Message(id, input);
-  },
+  ip: function (args, request) {
+    return request.ip;
+  }
 };
 
 var app = express();
+app.use(loggingMiddleware);
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
   graphiql: true,
 }));
-app.listen(4000, () => {
-  console.log('Running a GraphQL API server at localhost:4000/graphql');
-});
-
-/* mutation {
-  createMessage(input: {
-    author: "andy",
-    content: "hope is a good thing",
-  }) {
-    id
-  }
-} */
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
